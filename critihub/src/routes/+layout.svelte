4f5449aa98ee375dd17fa559e6e83dda3f51290a
Armando Jair Ibañez Parra <jairibaparr@outlook.com>
2024-05-03 09:29:04 -0700

    creando proyecto en svelte
    Moví todo lo creado a /old y empece el proyecto con svelte

---
+++ critihub/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/perfil">perfil</a>
+        </li>
+        <li>
+            <a href="/login">login</a>
+        </li>
+        <li>
+            <a href="/about">about</a>
+        </li>
+        <li>
+            <a href="/#">logout</a>
+        </li>            
+    </ul>
+</nav>
+<slot></slot>
