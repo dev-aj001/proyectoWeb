67857d812bc52f423299bcceb4ced48192b2733a
Cesar Hernandez <149346749+cesarurielhr@users.noreply.github.com>
2024-05-02 19:03:53 -0700

    web
    primera versión

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# proyectoWeb
+# Crear un Formulario de Login con HTML y CSS
+Como crear un simple y sencillo formulario de Login usando solamente HTML y CSS, y veremos un resultado excelente.
---
+++ config.js
@@ -0,0 +1,22 @@
+// conexion con firebase
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBvwYX0AX7V1jCygsiuktfH59QAFfBvTZU",
+    authDomain: "critichub-27650.firebaseapp.com",
+    projectId: "critichub-27650",
+    storageBucket: "critichub-27650.appspot.com",
+    messagingSenderId: "316576317047",
+    appId: "1:316576317047:web:9bb4749d2af711d8769072",
+    measurementId: "G-ZLT30NQGXY"
+  };
+  
+  // Inicializacion de Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Inicializacio con Cloud Firestore (Database) y obtener una referencia del servicio
+  const db = firebase.firestore();
+
+
+  function login() {
+  
+}
