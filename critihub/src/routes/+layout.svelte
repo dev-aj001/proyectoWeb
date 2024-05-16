f83775e73f9e6027606b6f4623f37bbfab32a919
Armando Jair Ibañez Parra <jairibaparr@outlook.com>
2024-05-16 00:21:56 -0700

    New Svelte project with boostrap
    Se creo de nuevo el proyecto en svelte 4.2.7  ahora con boostrap 5.3.3

---
+++ critihub/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<slot></slot><style></style><slot></slot><script>import { browser } from "$app/environment";
+import { onMount } from "svelte";
+import "bootstrap/dist/css/bootstrap.css";
+
+onMount(async () => {
+  if (!browser) return;
+
+  // this is enough for most components
+  await import("bootstrap");
+
+  // some components require a bootstrap instance, to fulfil their job. In that case, use this:
+  // const bootstrap = await import("bootstrap");
+  // sample usage: 
+  // const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+});</script><style></style>
